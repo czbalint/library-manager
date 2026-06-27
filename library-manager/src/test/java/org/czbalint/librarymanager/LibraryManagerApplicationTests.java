55f97078030f98d8bacf947b6a1a73900a5b9722
czbalint <62812514+czbalint@users.noreply.github.com>
2026-06-28 00:04:18 +0200

    init commit

---
+++ library-manager/src/test/java/org/czbalint/librarymanager/LibraryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.czbalint.librarymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
